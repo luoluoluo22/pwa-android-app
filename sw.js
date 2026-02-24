899dbb3f5ee3aec43a746cf85d47d7cac4b231ea
luoluoluo22 <1137583371@qq.com>
2026-02-24 21:36:49 +0800

    Fix: Add required PNG icons for PWA installability

--- sw.js
+++ sw.js
@@ -5,7 +5,9 @@ const ASSETS = [
     './style.css',
     './app.js',
     './manifest.json',
-    './icon.svg'
+    './icon.svg',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
 // Install Event
